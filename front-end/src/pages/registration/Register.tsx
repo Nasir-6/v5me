f2aa087faf0e72eb2358570f6b7467ed8f1f33b4
Nasir <nasir-islam@hotmail.com>
2022-07-11 23:37:48 +0100

    add checkbox and styles for register_user page

--- front-end/src/pages/registration/Register.tsx
+++ front-end/src/pages/registration/Register.tsx
@@ -1,16 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { RegisterUser } from "./RegisterUser";
 import { RegisterCar } from "./RegisterCar";
 import { RegisterMechanic } from "./RegisterMechanic";
+import logo from '../../assets/v5me-logo-white.png'
 
-
-interface RegisterProps {
-
-}
+interface RegisterProps {}
 
 export const Register: React.FC<RegisterProps> = ({}) => {
-
-    const [page, setPage] = useState(0);
+  const [page, setPage] = useState(0);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -22,55 +19,48 @@ export const Register: React.FC<RegisterProps> = ({}) => {
     other: "",
   });
 
+  const PageDisplay = () => {
+    if (page === 0) {
+      // return <RegisterUser formData={formData} setFormData={setFormData} />;
+      return <RegisterUser />;
+    } else if (page === 1) {
+      return <RegisterCar />;
+    } else {
+      return <RegisterMechanic />;
+    }
+  };
 
-    const FormTitles = ["User Details", "Car Details", "Mechanic Details"];
+  return (
+    <div className="register_page flex-column">
+      <a href="/">
+        <img src={logo} alt="" className="logo" />
+      </a>
 
-    const PageDisplay = () => {
-      if (page === 0) {
-        // return <RegisterUser formData={formData} setFormData={setFormData} />;
-        return <RegisterUser />;
-      } else if (page === 1) {
-        return <RegisterCar />;
-      } else {
-        return <RegisterMechanic />;
-      }
-    };
-  
-    return (
-      <div className="form">
-        <div className="progressbar">
-          <div
-            style={{ width: page === 0 ? "33.3%" : page == 1 ? "66.6%" : "100%" }}
-          ></div>
-        </div>
-        <div className="form-container">
-          <div className="header">
-            <h1>{FormTitles[page]}</h1>
-          </div>
-          <div className="body">{PageDisplay()}</div>
-          <div className="footer">
-            <button
-              disabled={page == 0}
-              onClick={() => {
-                setPage((currPage) => currPage - 1);
-              }}
-            >
-              Prev
-            </button>
-            <button
-              onClick={() => {
-                if (page === FormTitles.length - 1) {
-                  alert("FORM SUBMITTED");
-                  console.log(formData);
-                } else {
-                  setPage((currPage) => currPage + 1);
-                }
-              }}
-            >
-              {page === FormTitles.length - 1 ? "Submit" : "Next"}
-            </button>
-          </div>
-        </div>
+      {PageDisplay()}
+      <div className="register_page-btns">
+        <button
+          className="btn"
+          disabled={page == 0}
+          onClick={() => {
+            setPage((currPage) => currPage - 1);
+          }}
+        >
+          Prev
+        </button>
+        <button
+          className="btn"
+          onClick={() => {
+            if (page === 2) {
+              alert("FORM SUBMITTED");
+              console.log(formData);
+            } else {
+              setPage((currPage) => currPage + 1);
+            }
+          }}
+        >
+          {page === 2 ? "Submit" : "Next"}
+        </button>
       </div>
-    );
-}
+    </div>
+  );
+};
--- front-end/src/pages/registration/RegisterUser.tsx
+++ front-end/src/pages/registration/RegisterUser.tsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-interface RegisterUserProps {
-
-}
+interface RegisterUserProps {}
 
 export const RegisterUser: React.FC<RegisterUserProps> = ({}) => {
-        return (
-            <h1>Register User</h1>
-        );
-}
+
+    const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
+  return (
+    <div>
+      
+      <form action="" className="register_user_form flex-column">
+      <h1>Register User</h1>
+        <input
+          type="text"
+          name="first_name"
+          id="first_name_input"
+          placeholder="First Name"
+          className="input_field"
+        />
+        <input
+          type="text"
+          name="last_name"
+          id="last_name_input"
+          placeholder="Last Name"
+          className="input_field"
+        />
+
+<label htmlFor='mechanic_check_input' className="checkbox_container" >
+        Are you registering as a Mechanic?
+        <input type="checkbox" name="mechanic_check" id="mechanic_check_input"
+        checked={checked}
+        onClick={handleChange}/>
+        <span className="checkmark"></span>
+    </label>
+    
+
+    {/* <div className="flex-row"> */}
+
+    <label htmlFor='car_owner_check_input' className="checkbox_container" >
+        Are you registering as a vehicle owner?
+        <input type="checkbox" name="car_owner_check" id="car_owner_check_input"
+        checked={checked}
+        onClick={handleChange}/>
+        <span className="checkmark"></span>
+    </label>
+
+
+
+
+      </form>
+    </div>
+  );
+};
