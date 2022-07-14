531f750e891e0210a1abf0fbd0c2e2e9e92eb621
Nasir <nasir-islam@hotmail.com>
2022-07-14 16:25:14 +0100

    setup useEffect

--- front-end/src/pages/registration/RegisterCar.tsx
+++ front-end/src/pages/registration/RegisterCar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface RegisterCarProps {
     userData: UserData;
@@ -27,7 +27,31 @@ export const RegisterCar: React.FC<RegisterCarProps> = ({userData,
     carData,
     setCarData,
     setPage,}) => {
+
+        const [regNumber, setRegNumber] = useState(carData.regNumber);
+        const [make, setMake] = useState(carData.make);
+        const [engineSize, setEngineSize] = useState(carData.engineSize);
+        const [nickName, setNickName] = useState(carData.nickName);
+
+        // CheckBoxes
+        const [ownerHasAgreed, setOwnerHasAgreed] = useState(carData.ownerHasAgreed);
+
+        const handleOwnerHasAgreedCheckbox = () => {
+          setOwnerHasAgreed(!ownerHasAgreed);
+        };
+
+        useEffect(() => {
+            setCarData({
+              regNumber: regNumber,
+              make: make,
+              engineSize: engineSize,
+              nickName: nickName,
+              ownerHasAgreed: ownerHasAgreed
+            });
+          }, [ regNumber, make, engineSize, nickName, ownerHasAgreed]);
+
         return (
             <h1>Register Car</h1>
         );
+        
 }
